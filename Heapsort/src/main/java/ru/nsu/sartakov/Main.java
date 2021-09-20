b9367036c4f4c03a97f704dfed96b65ce4e5158b
zelebo <sartakov.sasha@icloud.com>
2021-09-20 21:25:49 +0700

    with stupid input

--- Heapsort/src/main/java/ru/nsu/sartakov/Main.java
+++ Heapsort/src/main/java/ru/nsu/sartakov/Main.java
@@ -1,21 +1,24 @@
 package ru.nsu.sartakov;
-
-// get the variables (in string args)
-// write the array
-// make the heapsort
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {12, 11, 13, 5, 6, 7};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
 
         Heapsort hs = new Heapsort();
         hs.sort(arr);
 
-
-
         // print the array
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + "\n");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + ", ");
         }
+        System.out.println(arr[arr.length - 1]);
     }
 }
