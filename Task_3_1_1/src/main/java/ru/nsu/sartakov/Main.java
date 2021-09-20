a2a04f2b0e08b8b618ed512267bfc71a926dace4
zelebo <sartakov.sasha@icloud.com>
2021-09-21 00:21:20 +0700

    Adding some tests: 1) example test 2) empty array 3) negative numbers 4) border numbers

--- Task_3_1_1/src/main/java/ru/nsu/sartakov/Main.java
+++ Task_3_1_1/src/main/java/ru/nsu/sartakov/Main.java
@@ -26,6 +26,8 @@ public class Main {
         for (int i = 0; i < arr.length - 1; i++) {
             System.out.print(arr[i] + ", ");
         }
-        System.out.println(arr[arr.length - 1]);
+        if (arr.length > 0) {
+            System.out.println(arr[arr.length - 1]);
+        }
     }
 }
