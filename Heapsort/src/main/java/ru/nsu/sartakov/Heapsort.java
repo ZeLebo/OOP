ab56e995cc8465ba2ef559fef29fadc9fa309b18
zelebo <sartakov.sasha@icloud.com>
2021-09-20 21:13:23 +0700

    Without input

--- Heapsort/src/main/java/ru/nsu/sartakov/Heapsort.java
+++ Heapsort/src/main/java/ru/nsu/sartakov/Heapsort.java
@@ -1,50 +1,40 @@
 package ru.nsu.sartakov;
 
-public class Heapsort {
-    // To heapify a subtree rooted with node in which is
-    // an index in arr[]. n is size of heap
-    public static void heapify(int arr[], int n, int i) {
-        int largest = i; // Initialize largest as root
-        int l = 2 * i + 1; // left = 2*i + 1
-        int r = 2 * i + 2; // right = 2*i + 2
-
-        // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-
-        // If right child is larger than largest so far
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
-
-        // If largest is not root
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
-            // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
-        }
-    }
-
-    public static Heapsort sort(int[] arr) {
+public class Heapsort{
+    public void sort(int arr[]) {
         int n = arr.length;
 
-        // Build heap (rearrange array)
         for (int i = n / 2 - 1; i >= 0; i--) {
             heapify(arr, n, i);
         }
 
-        // One by one extract an element from heap
-        for (int i = n - 1; i > 0; i--) {
-            // Move current root to end
+        for (int i = n - 1; i >= 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
-
-            // call max heapify on the reduced heap
             heapify(arr, i, 0);
         }
-        return null;
+    }
+
+    void heapify(int arr[], int n, int i) {
+        int max = i;
+        int left = 2 * i + 1;
+        int right = left + 1;
+
+        if (left < n && arr[left] > arr[max]){
+            max = left;
+        }
+
+        if (right < n && arr[right] > arr[max]) {
+            max = right;
+        }
+
+        if (max != i) {
+            int tmp_1 = arr[i];
+            arr[i] = arr[max];
+            arr[max] = tmp_1;
+
+            heapify(arr, n, max);
+        }
     }
 }
--- Heapsort/src/main/java/ru/nsu/sartakov/Main.java
+++ Heapsort/src/main/java/ru/nsu/sartakov/Main.java
@@ -1,14 +1,20 @@
 package ru.nsu.sartakov;
 
+// get the variables (in string args)
+// write the array
+// make the heapsort
+
 public class Main {
     public static void main(String[] args) {
         int arr[] = {12, 11, 13, 5, 6, 7};
-        int n = arr.length;
 
-        Heapsort ob = Heapsort.sort(arr);
-        Heapsort.sort(arr);
+        Heapsort hs = new Heapsort();
+        hs.sort(arr);
+
+
+
         // print the array
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i] + "\n");
         }
     }
