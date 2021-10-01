227d6358926eac4bf305de55b0d6a38caf9cf508
= <sartakov.sasha@icloud.com>
2021-10-01 17:12:38 +0700

    some refactor and checks started writting tests

--- Task_1_1_2/src/main/java/ru/nsu/sartakov/ZFunc.java
+++ Task_1_1_2/src/main/java/ru/nsu/sartakov/ZFunc.java
@@ -1,6 +1,32 @@
 package ru.nsu.sartakov;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+
+/*
+input.txt - file where to find
+pattern - what to find
+*/
 public class ZFunc {
+
+    public static StringBuilder startFunc(String file_name, File file, String pattern) throws IOException {
+        // reading the file in buffer
+        final int BUF_LENGTH = 2048;
+        char[] buf = new char[BUF_LENGTH];
+        // Reader in UTF_8
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file_name), StandardCharsets.UTF_8), BUF_LENGTH);
+        StringBuilder res = new StringBuilder();
+        for (int shift = 0; shift < (int) file.length(); shift += BUF_LENGTH - pattern.length()) {
+            int charsRead = reader.read(buf);
+            String toCheck = new String(Arrays.copyOf(buf, shift + charsRead - 1));
+            res.append(search(shift, toCheck, pattern));
+        }
+        return res;
+    }
+
     public static StringBuilder search(int shift, String text, String pattern) {
         StringBuilder res = new StringBuilder();
         String concat = pattern + "$" + text;
@@ -37,5 +63,29 @@ public class ZFunc {
                 }
             }
         }
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        String file_name = input.nextLine();
+
+        if (file_name.length() == 0){
+            System.out.println("Enter something UwU");
+            return;
+        }
+        file_name = "src/files/" + file_name;
+
+        File file = new File (file_name);
+        if (!file.exists()) {
+            System.out.println("There's no such file");
+        }
+
+
+        String pattern = input.nextLine();
+        if ( pattern.length() == 0) {
+            System.out.println("I don't know what to find");
+            return;
+        }
+        StringBuilder res = startFunc(file_name, file, pattern);
+        System.out.println(res);
     }
 }
--- Task_1_1_2/src/test/java/ru/nsu/sartakov/ZFuncTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/sartakov/ZFuncTest.java
@@ -1,7 +1,41 @@
 package ru.nsu.sartakov;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZFuncTest {
+    private static void testFunc (String Text, String pattern) {
 
+    }
+
+    @Test
+    public void ZFunc_emptyFile() {
+
+    }
+
+    @Test
+    public void ZFunc_emptyPattern() {
+
+    }
+
+    @Test
+    public void ZFunc_bigFile() {
+
+    }
+
+    @Test
+    public void ZFunc_patternLenEqOne() {
+
+    }
+
+    @Test
+    public void ZFunc_normalFile() {
+
+    }
+
+    @Test
+    public void ZFunc_noSubstrings() {
+
+    }
 }
