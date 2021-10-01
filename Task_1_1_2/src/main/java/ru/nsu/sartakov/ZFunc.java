d3c2752c82ef1d2e57bbc7dcbacf10c2f0f029f2
= <sartakov.sasha@icloud.com>
2021-10-01 18:23:10 +0700

    Tests are not working

--- Task_1_1_2/src/main/java/ru/nsu/sartakov/ZFunc.java
+++ Task_1_1_2/src/main/java/ru/nsu/sartakov/ZFunc.java
@@ -5,30 +5,53 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
+import java.util.SplittableRandom;
 
 /*
 input.txt - file where to find
 pattern - what to find
 */
 public class ZFunc {
-
     public static StringBuilder startFunc(String file_name, File file, String pattern) throws IOException {
+        StringBuilder res = new StringBuilder();
+
+        // some checkups for input
+        if (!file.exists()) {
+            String resNoFile = "There's no such file";
+            res.append(resNoFile);
+            return res;
+        }
+        if ( pattern.length() == 0) {
+            String resNothing = "I don't know what to find";
+            res.append(resNothing);
+            return res;
+        }
+
+        if (file.length() == 0) {
+            String resEmpty = "The file is empty";
+            res.append(resEmpty);
+            return res;
+        }
+
         // reading the file in buffer
         final int BUF_LENGTH = 2048;
         char[] buf = new char[BUF_LENGTH];
-        // Reader in UTF_8
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file_name), StandardCharsets.UTF_8), BUF_LENGTH);
-        StringBuilder res = new StringBuilder();
+        // make it work with shifts
         for (int shift = 0; shift < (int) file.length(); shift += BUF_LENGTH - pattern.length()) {
             int charsRead = reader.read(buf);
+            // start the search function ( ZFunc logic )
             String toCheck = new String(Arrays.copyOf(buf, shift + charsRead - 1));
             res.append(search(shift, toCheck, pattern));
         }
+        res.insert(0, '{');
+        res.setCharAt((res.length() - 1), '}');
         return res;
     }
 
     public static StringBuilder search(int shift, String text, String pattern) {
         StringBuilder res = new StringBuilder();
+        // making it one string
         String concat = pattern + "$" + text;
         int[] Z = new int[concat.length()];
         getZarr(concat, Z);
@@ -39,32 +62,33 @@ public class ZFunc {
         }
         return res;
     }
+
     private static void getZarr(String str, int[] Z) {
         int n = str.length();
         int L = 0, R = 0;
-        for(int i = 1; i < n; ++i) {
-            if(i > R){
-                L = R = i;
-                while(R < n && str.charAt(R - L) == str.charAt(R))
-                    R++;
+        for(int i = 1; i < n; i++) {
+            if (i > R) {
+                L = i;
+                for (R = i; R < n && str.charAt(R - L) == str.charAt(R); R++);
                 Z[i] = R - L;
                 R--;
             }
-            else{
+            else {
                 int k = i - L;
-                if(Z[k] < R - i + 1)
+                if ( Z[k] < R - i + 1 )
                     Z[i] = Z[k];
                 else{
                     L = i;
-                    while(R < n && str.charAt(R - L) == str.charAt(R))
-                        R++;
+                    for ( ; R < n && str.charAt(R - L) == str.charAt(R); R++);
                     Z[i] = R - L;
                     R--;
                 }
             }
         }
     }
+
     public static void main(String[] args) throws IOException {
+        // IO scanner and some checkups
         Scanner input = new Scanner(System.in);
         String file_name = input.nextLine();
 
@@ -72,19 +96,11 @@ public class ZFunc {
             System.out.println("Enter something UwU");
             return;
         }
+        // rename for specific directory
         file_name = "src/files/" + file_name;
-
         File file = new File (file_name);
-        if (!file.exists()) {
-            System.out.println("There's no such file");
-        }
-
-
         String pattern = input.nextLine();
-        if ( pattern.length() == 0) {
-            System.out.println("I don't know what to find");
-            return;
-        }
+        // entry point and something to output
         StringBuilder res = startFunc(file_name, file, pattern);
         System.out.println(res);
     }
--- Task_1_1_2/src/test/java/ru/nsu/sartakov/ZFuncTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/sartakov/ZFuncTest.java
@@ -1,6 +1,11 @@
 package ru.nsu.sartakov;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,32 +15,48 @@ class ZFuncTest {
     }
 
     @Test
-    public void ZFunc_emptyFile() {
-
+    public void ZFunc_emptyFile() throws IOException {
+        String file_name = "src/files/empty.txt";
+        String pattern = "something";
+        File file = new File(file_name);
+        StringBuilder res = ZFunc.startFunc(file_name, file, pattern);
+        StringBuilder resEmpty = new StringBuilder();
+        resEmpty.append("The file is empty");
+        Assertions.assertSame(resEmpty, res);
     }
 
     @Test
-    public void ZFunc_emptyPattern() {
-
+    public void ZFunc_emptyPattern() throws IOException {
+        String file_name = "src/files/test.txt";
+        String pattern = "";
+        File file = new File(file_name);
+        StringBuilder res = ZFunc.startFunc(file_name, file, pattern);
+        StringBuilder resNothing = new StringBuilder();
+        resNothing.append("I don't know what to find");
+        Assertions.assertSame(resNothing, res);
     }
 
     @Test
-    public void ZFunc_bigFile() {
-
+    public void ZFunc_bigFile() throws IOException {
+        String file_name = "src/files/bigfile.txt";
+        String pattern = "something";
+        File file = new File(file_name);
+        StringBuilder res = ZFunc.startFunc(file_name, file, pattern);
     }
 
     @Test
-    public void ZFunc_patternLenEqOne() {
-
+    public void ZFunc_normalFile() throws IOException {
+        String file_name = "src/files/empty.txt";
+        String pattern = "something";
+        File file = new File(file_name);
+        StringBuilder res = ZFunc.startFunc(file_name, file, pattern);
     }
 
     @Test
-    public void ZFunc_normalFile() {
-
-    }
-
-    @Test
-    public void ZFunc_noSubstrings() {
-
+    public void ZFunc_noSubstrings() throws IOException {
+        String file_name = "src/files/input.txt";
+        String pattern = "hello";
+        File file = new File(file_name);
+        StringBuilder res = ZFunc.startFunc(file_name, file, pattern);
     }
 }
