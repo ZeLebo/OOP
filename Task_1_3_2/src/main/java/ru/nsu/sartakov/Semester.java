6cdfc7bddcf52127114da898998e995dadb2c8da
= <sartakov.sasha@icloud.com>
2021-10-14 23:07:05 +0700

    WOP

---
+++ Task_1_3_2/src/main/java/ru/nsu/sartakov/Semester.java
@@ -0,0 +1,62 @@
+package ru.nsu.sartakov;
+
+import java.util.*;
+
+public class Semester {
+//    Semester() {};
+    private final Map<String, Integer> semester = new HashMap<>();
+
+    protected void setGrade(String course, int grade) {
+        this.semester.put(course, grade);
+    }
+
+    protected Collection<String> getCourses() {
+        return this.semester.keySet();
+    }
+
+    protected Collection<Integer> getGrades() {
+        return this.semester.values();
+    }
+
+    /**
+     * The logic of numbers
+     * @param marks is the marks itself
+     * @return result is the average grade for the semester
+     */
+
+    public static double averageScore(ArrayList<Integer> marks) {
+        double result = 0.0;
+
+        for (int mark : marks) {
+            result += mark;
+        }
+
+        result /= marks.size();
+        return result;
+    }
+
+    public static boolean Diploma(Notebook notebook) {
+        List<Integer> grades;
+        grades = notebook.getAllGrades();
+        long fives;
+        boolean satis = grades.stream().anyMatch(g -> g < 4);
+        fives = grades.stream().filter(g -> g == 5).count();
+        double fivesPercent = (double) fives / grades.size();
+
+        return fivesPercent >= 0.75 && !satis && notebook.getQualifyingWorkGrade() == 5;
+
+    }
+
+    public static boolean hasNoThree(Notebook notebook, int semester) {
+        Collection<Integer> marks = notebook.getSemesterGrades(semester);
+        boolean result = true;
+        for (int mark : marks) {
+            if (mark < 4) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+}
--- Task_1_3_2/src/test/java/ru/nsu/sartakov/NotebookTest.java
+++ Task_1_3_2/src/test/java/ru/nsu/sartakov/NotebookTest.java
@@ -1,7 +1,76 @@
 package ru.nsu.sartakov;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NotebookTest {
+    @Test
+    public void AverageTest() {
+        Notebook mine = new Notebook("SartakovAA", 5);
+        mine.add(1, "Algebra", 4);
+        mine.add(1, "Discrete", 4);
+        mine.add(1, "Declarative", 3);
+        mine.add(1, "Imperative", 3);
+        mine.add(1, "Russian", 5);
+        mine.add(1, "History", 5);
+
+        mine.add(2, "Algebra", 3);
+        mine.add(2, "Discrete", 5);
+        mine.add(2, "Declarative", 4);
+        mine.add(2, "Imperative", 4);
+        mine.add(2, "English", 5);
+        mine.add(2, "DP", 4);
+
+        ArrayList<Integer> grades = mine.getAllGrades(); //Here we can calculate the average score not only for the entire period.
+        Assertions.assertEquals(4.1, Semester.averageScore(grades), 0.1);
+    }
+
+    @Test
+    public void DiplomaTest() {
+
+        Notebook mine = new Notebook("SartakovAA", 5);
+        mine.add(1, "Algebra", 4);
+        mine.add(1, "Discrete", 4);
+        mine.add(1, "Declarative", 3);
+        mine.add(1, "Imperative", 3);
+        mine.add(1, "Russian", 5);
+        mine.add(1, "History", 5);
+
+        mine.add(2, "Algebra", 3);
+        mine.add(2, "Discrete", 5);
+        mine.add(2, "Declarative", 4);
+        mine.add(2, "Imperative", 4);
+        mine.add(2, "English", 5);
+        mine.add(2, "DP", 4);
+
+        mine.setQualifyingWorkGrade(5);
+        Assertions.assertFalse(Semester.Diploma(mine));
+    }
+
+    @Test
+    public void hasNoThreeTest() {
+
+        Notebook mine = new Notebook("SartakovAA", 5);
+        mine.add(1, "Algebra", 4);
+        mine.add(1, "Discrete", 4);
+        mine.add(1, "Declarative", 3);
+        mine.add(1, "Imperative", 3);
+        mine.add(1, "Russian", 5);
+        mine.add(1, "History", 5);
+
+        mine.add(2, "Algebra", 3);
+        mine.add(2, "Discrete", 5);
+        mine.add(2, "Declarative", 4);
+        mine.add(2, "Imperative", 4);
+        mine.add(2, "English", 5);
+        mine.add(2, "DP", 4);
+
+        Assertions.assertFalse(Semester.hasNoThree(mine, 1));
+    }
 
 }
