b7e0e9a9657d981338f1c6b4638810c7e72a717d
= <sartakov.sasha@icloud.com>
2021-10-14 23:07:05 +0700

    WOP

--- Task_1_3_2/src/main/java/ru/nsu/sartakov/Notebook.java
+++ Task_1_3_2/src/main/java/ru/nsu/sartakov/Notebook.java
@@ -1,27 +1,59 @@
 package ru.nsu.sartakov;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
 
-/**
- * Текущий бал за все время
- * Можно ли получить красный диплом с отличием
- * Будет ли повышенная стипедия
- */
 
-/**
- * 75% оценок в приложении к диплому ( последняя оценка ) - "отлично"
- * Нет в зачетной книжке итоговых оценок "удовлетворительно"
- * Клалификационная работа защищена на "отлично"
- */
+public class Notebook extends Semester{
 
-public class Notebook {
-    int average = 0;
-    int[] marks;
+    private String fulName;
+    private String course;
+    private final Semester[] semesters = new Semester[9];
+    private int qualifyingWorkGrade;
 
-    public int getAverage() {
-        int res = 0;
-        return res;
+    public Notebook(String fulName, int qualifyingWorkGrade) {
+        this.fulName = fulName;
+        this.qualifyingWorkGrade = qualifyingWorkGrade;
+
+        for (int i = 1; i < 9; i++) {
+            Semester tmp = new Semester();
+            semesters[i] = tmp;
+        }
     }
 
+    public Semester[] getSemesters() {
+        return semesters;
+    }
 
+    public void setFulName(String fulName) {
+        this.fulName = fulName;
+    }
+
+    public String getFulName() {
+        return fulName;
+    }
+
+    public void add(int semester, String course, int grade) {
+        this.semesters[semester].setGrade(course, grade);
+    }
+
+    public Collection<Integer> getSemesterGrades(int course) {
+        return semesters[course].getGrades();
+    }
+
+    public void setQualifyingWorkGrade(int qualifyingWorkGrade) {
+        this.qualifyingWorkGrade = qualifyingWorkGrade;
+    }
+
+    public int getQualifyingWorkGrade() {
+        return this.qualifyingWorkGrade;
+    }
+
+    public ArrayList<Integer> getAllGrades() {
+        ArrayList<Integer> grades = new ArrayList<>();
+        for (int i = 1; i < 9; i++) {
+            grades.addAll(semesters[i].getGrades());
+        }
+        return grades;
+    }
 }
