484274d1970f1b60b421f5286d72fb242c832893
= <sartakov.sasha@icloud.com>
2021-11-09 10:54:37 +0700

    some refactoring

--- Calculator/src/main/java/ru/nsu/sartakov/Calculator.java
+++ Calculator/src/main/java/ru/nsu/sartakov/Calculator.java
@@ -5,16 +5,6 @@ import ru.nsu.sartakov.operations.*;
 import java.util.*;
 
 public class Calculator {
-    private final Map<String, Operation> operations = new HashMap<>();
-
-    public Calculator(){
-        List<Operation> list = OperationFabric.produceOperations();
-        for(Operation op : list){
-            operations.put(op.getRepresentation(), op);
-        }
-    }
-
-
     private boolean isNumeric(String input) {
         if (input == null) {
             return false;
@@ -33,14 +23,15 @@ public class Calculator {
      * @return the result of expression or Exception
      */
     public double calc(String inputString) {
+        OperationFabric fabric = new OperationFabric();
         String[] input = inputString.split(" ");
         Stack <Double> stack = new Stack<>();
 
         for (int i = input.length - 1; i > -1 ; --i) {
             if (isNumeric(input[i])) {
                 stack.push(Double.parseDouble(input[i]));
-            } else if (operations.containsKey(input[i])) {
-                stack.push(operations.get(input[i]).calculate(stack));
+            } else if (fabric.getOperations().containsKey(input[i])) {
+                stack.push(fabric.getOperations().get(input[i]).calculate(stack));
             } else {
                 throw new IllegalArgumentException("The operation wasn't found");
             }
--- Calculator/src/main/java/ru/nsu/sartakov/OperationFabric.java
+++ Calculator/src/main/java/ru/nsu/sartakov/OperationFabric.java
@@ -2,20 +2,35 @@ package ru.nsu.sartakov;
 
 import ru.nsu.sartakov.operations.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OperationFabric {
+    private final Map<String, Operation> operations = new HashMap<>();
+
+    public OperationFabric() {
+        List<Operation> list = OperationFabric.produceOperations();
+        for (Operation op : list) {
+            operations.put(op.getRepresentation(), op);
+        }
+    }
+
     public static List<Operation> produceOperations() {
-        List<Operation> lst = new ArrayList<>();
-        lst.add(new AddOperator());
-        lst.add(new CosOperator());
-        lst.add(new DivOperator());
-        lst.add(new LogOperator());
-        lst.add(new MultOperator());
-        lst.add(new PowOperator());
-        lst.add(new SinOperator());
-        lst.add(new SqrtOperator());
-        lst.add(new SubOperator());
-        return lst;
+        List<Operation> list = new ArrayList<>();
+        list.add(new AddOperator());
+        list.add(new CosOperator());
+        list.add(new DivOperator());
+        list.add(new LogOperator());
+        list.add(new MultOperator());
+        list.add(new PowOperator());
+        list.add(new SinOperator());
+        list.add(new SqrtOperator());
+        list.add(new SubOperator());
+        return list;
+    }
+
+    public Map<String, Operation> getOperations() {
+        return operations;
     }
 }
