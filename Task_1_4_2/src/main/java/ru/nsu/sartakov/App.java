30ec4e0031aefbaf14ff026919bee965b75b66af
= <sartakov.sasha@icloud.com>
2021-11-25 11:47:20 +0700

    Supposed to be working from IDEA

--- Task_1_4_2/src/main/java/ru/nsu/sartakov/App.java
+++ Task_1_4_2/src/main/java/ru/nsu/sartakov/App.java
@@ -1,17 +1,9 @@
 package ru.nsu.sartakov;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.apache.commons.cli.*;
 
 public class App {
@@ -40,42 +32,45 @@ public class App {
         CommandLineParser parser = new DefaultParser();
         CommandLine line = parser.parse(options, args);
 
-        if (line.hasOption("test")) {
-            System.out.println("Testing this out");
-
-            Gson gson = new Gson();
-            Reader reader = Files.newBufferedReader(Paths.get("Notes.json"));
-
-            List<Note> note = new Gson().fromJson(reader,new TypeToken<List<Note>>(){}.getType());
-            reader.close();
-
-            System.out.println(note);
-        }
-
         if (line.hasOption("add")) {
             String[] values = line.getOptionValues("add");
             notebook.addNote(values[0], values[1]);
-        }
-
-        if (line.hasOption("rm")) {
+        } else if (line.hasOption("rm")) {
             notebook.removeNote(line.getOptionValue("rm"));
-        }
-
-        if (line.hasOption("show")) {
+        } else if (line.hasOption("show")) {
             String[] values = line.getOptionValues("show");
             if (values == null) {
-                notebook.showAllNotes();
-                return;
+                List<Note> notes =  notebook.showAllNotes();
+                for (Note i : notes) {
+                    System.out.println(i.getTime());
+                    System.out.println(i.getHeading());
+                    System.out.println(i.getNote());
+                }
             }  else if (values.length == 2) {
-                notebook.showNotesPeriod(LocalDateTime.parse(values[0]), LocalDateTime.parse(values[1]));
+                List<Note> notes = notebook.showNotesPeriod(LocalDateTime.parse(values[0]), LocalDateTime.parse(values[1]));
+                for (Note i : notes) {
+                    System.out.println(i.getTime());
+                    System.out.println(i.getHeading());
+                    System.out.println(i.getNote());
+                }
+
             } else {
                 List<String> subWords = Arrays.stream(values).toList();
                 subWords.remove(0);
                 subWords.remove(0);
-                notebook.showNotes(LocalDateTime.parse(values[0]),
+                List<Note> notes = notebook.findNotesPeriodSwords(LocalDateTime.parse(values[0]),
                                 LocalDateTime.parse(values[1]),
                                 subWords);
+
+                for (Note i : notes) {
+                    System.out.println(i.getTime());
+                    System.out.println(i.getHeading());
+                    System.out.println(i.getNote());
+                }
             }
+        } else {
+            // TODO put the help function
+            System.out.println("You're doing something wrong");
         }
     }
 }
