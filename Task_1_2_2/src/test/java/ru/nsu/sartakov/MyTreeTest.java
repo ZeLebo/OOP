1ac53d3506c9680ff3aed264162927766091a900
= <sartakov.sasha@icloud.com>
2021-12-11 20:49:09 +0700

    WIP

---
+++ Task_1_2_2/src/test/java/ru/nsu/sartakov/MyTreeTest.java
@@ -0,0 +1,20 @@
+package ru.nsu.sartakov;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MyTreeTest {
+
+    @Test
+    void add() {
+        MyTree tree = new MyTree(3);
+        tree.add(new int[]{1, 2, 3, 4});
+    }
+
+    @Test
+    void remove() {
+        MyTree tree = new MyTree(3);
+        tree.add("Hello");
+        Assertions.assertEquals(tree.getClass(), tree.getClass());
+    }
+}
