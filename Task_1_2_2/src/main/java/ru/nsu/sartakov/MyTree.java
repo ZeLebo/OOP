44b50735a266a5e2ac05f166e6298d9fbfe5cc35
= <sartakov.sasha@icloud.com>
2021-12-11 20:49:09 +0700

    WIP

---
+++ Task_1_2_2/src/main/java/ru/nsu/sartakov/MyTree.java
@@ -0,0 +1,27 @@
+package ru.nsu.sartakov;
+
+public class MyTree {
+    /**
+     *
+     * @param n the amount of leaves
+     */
+    MyTree(int n){}
+
+    /**
+     *
+     * @param value object to store in the tree
+     * @return true if added, false otherwise
+     */
+    public boolean add(Object value) {
+        return true;
+    }
+
+    /**
+     *
+     * @param value to be deleted
+     * @return whether succeed or not
+     */
+    public boolean remove(Object value) {
+        return true;
+    }
+}
