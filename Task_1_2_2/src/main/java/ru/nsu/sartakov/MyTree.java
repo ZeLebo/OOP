75db7b9c1461619ff132d1c5628ef9cdf8fc5747
= <sartakov.sasha@icloud.com>
2021-12-14 05:04:10 +0700

    WIP

--- Task_1_2_2/src/main/java/ru/nsu/sartakov/MyTree.java
+++ Task_1_2_2/src/main/java/ru/nsu/sartakov/MyTree.java
@@ -1,27 +1,63 @@
 package ru.nsu.sartakov;
 
-public class MyTree {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyTree<T> implements Iterable<T> {
+    // some thought about that lab
+    public static class TreeNode {
+        int val;
+        List<TreeNode> children = new LinkedList<>();
+
+        TreeNode(int data) {
+            val = data;
+        }
+
+        TreeNode(int data, List<TreeNode> child) {
+            val = data;
+            children = child;
+        }
+    }
     /**
-     *
      * @param n the amount of leaves
      */
+    private T[] treeArr;
     MyTree(int n){}
 
     /**
-     *
      * @param value object to store in the tree
      * @return true if added, false otherwise
      */
-    public boolean add(Object value) {
+    public boolean add(T value) {
         return true;
     }
 
     /**
-     *
      * @param value to be deleted
      * @return whether succeed or not
      */
-    public boolean remove(Object value) {
+    public boolean remove(T value) {
         return true;
+    }
+
+    private class Iter implements Iterator<T> {
+        int actPos = 0;
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public T next() {
+            actPos++;
+            return treeArr[actPos];
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iter();
     }
 }
