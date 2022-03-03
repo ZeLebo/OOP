4e8f2cd8b6719a131934582cc6b08aad9149de1a
= <sartakov.sasha@icloud.com>
2022-03-03 22:05:41 +0700

    I have never read that much information per hour

--- Task_2_1_1/src/main/java/ru/nsu/sartakov/ThreadChecker.java
+++ Task_2_1_1/src/main/java/ru/nsu/sartakov/ThreadChecker.java
@@ -11,15 +11,6 @@ public class ThreadChecker {
         return numbersDeque.isEmpty() ? null : numbersDeque.pop();
     }
 
-    private Callable<Boolean> primeChecker() {
-        Long number;
-        while ((number = getNumber()) != null) {
-            if (Checker.notPrime(number)) {
-                return true;
-            }
-        }
-        return false;
-    }
     /**
      * Runs the check-up using multi-thread
      * @param array - set of numbers, needed to be checked
@@ -27,12 +18,35 @@ public class ThreadChecker {
      * @return true if it has no prime
      * @throws InterruptedException if the operations were interrupted
      */
-    public boolean threadRun(long[] array, int threadsNumber) throws InterruptedException {
+    public boolean threadRun(long[] array, int threadsNumber) throws InterruptedException, ExecutionException {
         if (threadsNumber > 0 && threadsNumber < THREADS) {
             THREADS = threadsNumber;
         }
-
         numbersDeque = new ArrayDeque<>(Arrays.stream(array).boxed().toList());
-        Callable<Boolean> thread = primeChecker();
+        Callable<Boolean> thread = () -> {
+            Long number;
+            while((number = getNumber()) != null) {
+                if(Checker.notPrime(number)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        List<Callable<Boolean>> threads = new ArrayList<>();
+        for (int i = 0; i < THREADS; i++) {
+            threads.add(thread);
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(THREADS);
+        List<Future<Boolean>> futureList = executorService.invokeAll(threads);
+        for (var fut : futureList) {
+            if (fut.get()) {
+                executorService.shutdown();
+                return true;
+            }
+        }
+        executorService.shutdownNow();
+        return false;
     }
 }
