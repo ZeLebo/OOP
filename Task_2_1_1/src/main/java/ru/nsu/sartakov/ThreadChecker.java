fc631c22e8eee1622b21d661799c8997c0127bab
= <sartakov.sasha@icloud.com>
2022-03-03 21:58:30 +0700

    AAAAAA

--- Task_2_1_1/src/main/java/ru/nsu/sartakov/ThreadChecker.java
+++ Task_2_1_1/src/main/java/ru/nsu/sartakov/ThreadChecker.java
@@ -6,8 +6,19 @@ import java.util.concurrent.*;
 public class ThreadChecker {
     int THREADS = Runtime.getRuntime().availableProcessors();
     private Deque<Long> numbersDeque;
+
     private synchronized Long getNumber() {
         return numbersDeque.isEmpty() ? null : numbersDeque.pop();
+    }
+
+    private Callable<Boolean> primeChecker() {
+        Long number;
+        while ((number = getNumber()) != null) {
+            if (Checker.notPrime(number)) {
+                return true;
+            }
+        }
+        return false;
     }
     /**
      * Runs the check-up using multi-thread
@@ -20,34 +31,8 @@ public class ThreadChecker {
         if (threadsNumber > 0 && threadsNumber < THREADS) {
             THREADS = threadsNumber;
         }
-        Thread[] threads = new Thread[THREADS];
-        arr = Arrays.copyOf(array, array.length);
 
-        for (int i = 0; i < THREADS; i++) {
-            threads[i] = new Thread(new PrimeCheck(i));
-            threads[i].start();
-        }
-        for (int i = 0; i < THREADS; i++) {
-            threads[i].join();
-        }
-        return noPrime;
+        numbersDeque = new ArrayDeque<>(Arrays.stream(array).boxed().toList());
+        Callable<Boolean> thread = primeChecker();
     }
-
-    public synchronized void hasNonePrime() {
-        noPrime = true;
-    }
-
-    class PrimeCheck implements Runnable {
-// todo split to chunk (range) to check
-        public void run() {
-            for (long l : arr) { // todo THREADS times check array
-                if (Checker.notPrime(l)) {
-                    hasNonePrime();
-                    break;
-                }
-            }
-        }
-    }
-
-
 }
