66961ae8baa3600f18823bb0ef5b42023c380f8c
= <sartakov.sasha@icloud.com>
2022-03-03 22:46:20 +0700

    Deleted extra function

--- Task_2_1_1/src/main/java/ru/nsu/sartakov/Checker.java
+++ Task_2_1_1/src/main/java/ru/nsu/sartakov/Checker.java
@@ -6,20 +6,6 @@ public class Checker {
      * @param number needed to be checked for primeness
      * @return true if number is prime
      */
-    public static boolean isPrime(long number) {
-        if (number <= 1) {
-            return false;
-        } else if (number <= 3) {
-            return true;
-        }
-        for (int i = 3; i < number; i++) {
-            if (number % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static boolean notPrime(long number) {
         if (number <= 1) {
             return true;
