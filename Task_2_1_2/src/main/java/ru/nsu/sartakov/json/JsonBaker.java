7dd0a26cd3053c02dfb58cf05c10ef5ec4c9d351
= <sartakov.sasha@icloud.com>
2022-03-28 13:38:36 +0700

    AAAAA

--- Task_2_1_2/src/main/java/ru/nsu/sartakov/json/JsonBaker.java
+++ Task_2_1_2/src/main/java/ru/nsu/sartakov/json/JsonBaker.java
@@ -1,11 +1,8 @@
 package ru.nsu.sartakov.json;
 
 import ru.nsu.sartakov.collections.BakerCollection;
-import ru.nsu.sartakov.employee.Baker;
 import ru.nsu.sartakov.entities.BakerEntity;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,11 +14,6 @@ public class JsonBaker extends Json implements BakerCollection {
         String data = readFile(FILENAME);
         List <BakerEntity> bakers = Arrays.asList(gson().fromJson(data, BakerEntity[].class));
         return new ArrayList<>(bakers);
-    }
-
-    private void setBakers(List<BakerEntity> bakers) {
-        String data = gson().toJson(bakers);
-        writeFile(FILENAME, data);
     }
 
     @Override
@@ -39,7 +31,6 @@ public class JsonBaker extends Json implements BakerCollection {
         BakerEntity baker = new BakerEntity(maxId + 1, cookingTime);
         bakers.add(baker);
 
-        setBakers(bakers);
         return baker;
     }
 }
