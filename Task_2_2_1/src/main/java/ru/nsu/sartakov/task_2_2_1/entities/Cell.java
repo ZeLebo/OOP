848c9bf934eb6ecc9e187c826a0283cbe48d2cf8
= <sartakov.sasha@icloud.com>
2022-05-05 12:27:14 +0700

    amen sh*t here we again

---
+++ Task_2_2_1/src/main/java/ru/nsu/sartakov/task_2_2_1/entities/Cell.java
@@ -0,0 +1,39 @@
+package ru.nsu.sartakov.task_2_2_1.entities;
+
+public class Cell {
+    public int x;
+    public int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell cell = (Cell) o;
+
+        if (x != cell.x) return false;
+        return y == cell.y;
+    }
+}
---
+++ Task_2_2_1/src/main/java/ru/nsu/sartakov/task_2_2_1/entities/Direction.java
@@ -0,0 +1,6 @@
+package ru.nsu.sartakov.task_2_2_1.entities;
+
+public enum Direction {
+    left, right, up, down
+}
+
