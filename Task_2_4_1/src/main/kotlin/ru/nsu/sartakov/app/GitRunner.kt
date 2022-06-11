518183be91fd2fdd4c33f740552edaa62f7d1268
= <sartakov.sasha@icloud.com>
2022-06-11 20:53:30 +0700

    something is coming

--- Task_2_4_1/src/main/kotlin/ru/nsu/sartakov/app/GitRunner.kt
+++ Task_2_4_1/src/main/kotlin/ru/nsu/sartakov/app/GitRunner.kt
@@ -2,26 +2,24 @@ package ru.nsu.sartakov.app
 
 import ru.nsu.sartakov.dsl.DSL
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 class GitRunner {
-    // fun to clone repo to the repos directory
-    // repo - url to the repo
-    // dir - name of task to test
-    fun clone(repo: String) {
-        val command = "cd ./repos; git clone $repo"
-        Runtime.getRuntime().exec(command)
-    }
-
     // clone repo to the repos folder and run tests
-    fun cloneAndRunTests(repo: String, dir: String) {
-        clone(repo)
-        val command = "cd repos/$dir && ./gradlew test; cd .. && rm -rf $dir"
-        Runtime.getRuntime().exec(command)
+    fun cloneAndRunTests(nick: String, repo: String, dir: String) {
+        // check if the repo is already cloned
+        val repoDir = File(dir, repo)
+        if (repoDir.exists()) {
+            println("Repo $repo already cloned")
+        } else {
+            Runtime.getRuntime().exec("git clone $repo repos/$nick").waitFor(2, TimeUnit.SECONDS)
+        }
+        println(Runtime.getRuntime().exec("cd repos/$nick/$dir && ./gradlew test"))
     }
 }
 
 fun main() {
     val runner = GitRunner()
     val dsl = DSL()
-    runner.cloneAndRunTests(dsl.student.url, dsl.student.givenTasks[0])
+    runner.cloneAndRunTests(dsl.student.nickName, dsl.student.url, dsl.student.givenTasks[0])
 }
--- Task_2_4_1/src/main/kotlin/ru/nsu/sartakov/dsl/DSL.kt
+++ Task_2_4_1/src/main/kotlin/ru/nsu/sartakov/dsl/DSL.kt
@@ -18,7 +18,7 @@ class DSL {
         nickName = "ZeLebo"
         firstName = "Alexander"
         lastName = "Sartakov"
-        url = "https://github.com/ZeLebo/OOP"
+        url = "https://github.com/ZeLebo/OOP.git"
 
         givenTasks("Task_1_1_1", "Task_1_1_2")
 
