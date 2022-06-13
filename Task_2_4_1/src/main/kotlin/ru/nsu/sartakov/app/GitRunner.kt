33c09d6b35e07940263ba2f0d79e82ce62062267
= <sartakov.sasha@icloud.com>
2022-06-13 18:41:07 +0700

    now the results of tests are outputed

--- Task_2_4_1/src/main/kotlin/ru/nsu/sartakov/app/GitRunner.kt
+++ Task_2_4_1/src/main/kotlin/ru/nsu/sartakov/app/GitRunner.kt
@@ -1,11 +1,12 @@
 package ru.nsu.sartakov.app
 
+import org.gradle.tooling.GradleConnector
 import ru.nsu.sartakov.dsl.DSL
 import ru.nsu.sartakov.entities.Student
 import java.io.File
 import java.util.concurrent.TimeUnit
-import org.gradle.tooling.GradleConnector
 
+typealias BuildTest = Pair<Boolean, Boolean>
 
 class GitRunner {
     /**
@@ -14,9 +15,11 @@ class GitRunner {
     private fun clone(student : Student) {
         val repoDir = File("./repos/${student.nickName}")
         if (!repoDir.exists()) {
-            Runtime.getRuntime().exec("git clone ${student.url} repos/${student.nickName}").waitFor(2, TimeUnit.SECONDS)
+            Runtime.getRuntime().exec("git clone ${student.url} repos/${student.nickName}").
+                waitFor(2, TimeUnit.SECONDS)
             // delete .git from it to prevent git from messing with it
-            Runtime.getRuntime().exec("rm -rf repos/${student.nickName}/.git").waitFor(2, TimeUnit.SECONDS)
+            Runtime.getRuntime().exec("rm -rf repos/${student.nickName}/.git").
+                waitFor(2, TimeUnit.SECONDS)
         }
     }
 
@@ -24,27 +27,45 @@ class GitRunner {
         TODO("I will generate docs for $student for $taskName later")
     }
 
-    fun runTests(student: Student, taskId : String) {
+    fun runTests(student: Student, taskId : String) : BuildTest {
         if (!File("./repos/${student.nickName}/$taskId").exists()) {
             println("You do not have a repository for task $taskId, cloning it...")
             clone(student)
         }
-        // todo how to count the tests and beaufitfy the output
-        val connection = GradleConnector.newConnector().forProjectDirectory(File("./repos/${student.nickName}/$taskId")).connect()
-        // run tests and get the result
-        connection.use {
-            connection.newBuild().forTasks("test").run().let {
-                println(it.toString())
+
+        // if build or tests are failed, we will return false
+        GradleConnector.newConnector().
+        forProjectDirectory(File("./repos/${student.nickName}/$taskId")).
+        connect().use { connection ->
+            var build = connection.newBuild().forTasks("build")
+
+            val buildRes = try {
+                build.run()
+                true
+            } catch (e: Exception) {
+                println("Build failed")
+                e.printStackTrace()
+                false
             }
+
+            build = connection.newBuild().forTasks("test")
+            val testRes = try {
+                build.run()
+                true
+            } catch (e: Exception) {
+                println("Test failed")
+                e.printStackTrace()
+                false
+            }
+
+            return BuildTest(buildRes, testRes)
         }
-        //connection.close()
-        println("Tests for task $taskId are done")
-        connection.close()
     }
 }
 
 fun main() {
     val runner = GitRunner()
     val dsl = DSL()
-    runner.runTests(dsl.student, "Task_1_1_1")
+    val test = runner.runTests(dsl.student, "Task_1_1_1")
+//     println(test.first)
 }
